307d328a88531729b244aced01ec8dfce2b9b166
Migle <miglea97@gmail.com>
2025-02-17 16:16:01 +0100

    feat: Operator creates a Job and updates the app Deployment to use the new DB Version, which triggers K8s Rolling Update.

--- db-update-operator/databaseUpdate.py
+++ db-update-operator/databaseUpdate.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 import kopf
 import kubernetes.client as k8s_client
 import kubernetes.config as k8s_config
@@ -11,9 +12,20 @@ k8s_config.load_incluster_config()
 # They are called when any CR is created, modified or deleted and define how the operator should respond to events.
 @kopf.on.create('kopf.dev', 'v1', 'databaseupdates')
 @kopf.on.update('kopf.dev', 'v1', 'databaseupdates')
-async def handle_database_update(spec, status, namespace, logger, **kwargs):
+async def handle_database_update(spec, status, name, namespace, logger, **kwargs):
     desired_version = spec.get('version')
-    current_version = spec.get('currentVersion', '')
+    current_version = status.get('currentVersion', '')
+
+    # A Job ensures (by monitoring) that a specified number of Pods
+    # run successfully until a given task is done f.eks. DB Migration. It has a start and an end.
+    # If failed, it restarts the Job until success or a defined retry limit is reached.
+    # It works by creating one or more pods to run a task.
+
+    # The Operator does the following:
+    # 1. Check current DB version with the desired DB version.
+    # 2. Trigger a Job by creating a manifest for a Job.
+    # 3. Create the Job.
+    # 4. Monitor the job by polling for the Job until it completes or fails.
 
     # Step 1: Check current DB version with the desired DB version.
     if desired_version == current_version:
@@ -21,21 +33,10 @@ async def handle_database_update(spec, status, namespace, logger, **kwargs):
 
         return {'currentVersion': current_version}
 
-    # A Job ensures (by monitoring) that a specified number of Pods
-    # run successfully until a given task is done f.eks. DB Migration. It has a start and an end.
-    # If failed, it restarts the Job until success or a defined retry limit is reached.
-    # It works by creating one or more pods to run a task.
+    # Step 2: Trigger a Job by creating a manifest for a Job.
+    logger.info(f"Database update required: {current_version} -> {desired_version}")
 
-    # The Operator should:
-    # 1. Trigger the Job.
-    # 2. Monitor its progress and wait for it to finnish.
-    # 3. Update the custom resource's status.
-    # 4. Update the Deployment.
-
-    # Step 2: Create a manifest for a Job.
-    logger.info("Database update required: %s -> %s", current_version, desired_version)
-
-    job_name = f"{name}-job"
+    job_name = f"{name}-{desired_version}-job"
     job_manifest = {
         "apiVersion": "batch/v1",
         "kind": "Job",
@@ -47,6 +48,7 @@ async def handle_database_update(spec, status, namespace, logger, **kwargs):
             }
         },
         "spec": {
+            "ttlSecondsAfterFinished": 200, # This deletes the job automatically after 200 secs, both if Success or Fail.
             "template": {
                 "metadata": {
                     "labels": {
@@ -59,7 +61,7 @@ async def handle_database_update(spec, status, namespace, logger, **kwargs):
                     "containers": [
                         {
                             "name": "db-update",
-                            "image": "migliuss/job-script-image:latest",
+                            "image": "migliuss/job-script-image",
                             "env": [
                                 # Add any additional environment variables (e.g., credentials)
                                 {
@@ -78,7 +80,7 @@ async def handle_database_update(spec, status, namespace, logger, **kwargs):
     # Use BatchV1Api for Job creation: https://github.com/kubernetes-client/python/blob/master/kubernetes/docs/BatchV1Api.md#patch_namespaced_job
     batch_v1 = k8s_client.BatchV1Api()
     try:
-        batch_v1.create_namespaced_jon(namespace, job_manifest)
+        batch_v1.create_namespaced_job(namespace=namespace, body=job_manifest)
         logger.info(f"Job {job_name} created successfully")
     except ApiException as e:
         if e.status == 409:
@@ -90,18 +92,58 @@ async def handle_database_update(spec, status, namespace, logger, **kwargs):
     # Step 4: Poll for the Job until it completes or fails.
     for _ in range(36):  # Polling the job status every 5 seconds, up to 3 minutes. May need to increase depending on how long it takes to finnish the Job.
         job = batch_v1.read_namespaced_job(name=job_name, namespace=namespace)
-        # job.status.succeeded returns an int which is the number of pods which reached phase Succeeded.
+        # job.status.succeeded returns an int which is the number of pods which reached phase "Succeeded".
         # https://github.com/kubernetes-client/python/blob/master/kubernetes/docs/V1JobStatus.md
         if job.status.succeeded and job.status.succeeded >= 1:
             logger.info(f"Job {job_name} succeeded")
             break
-        time.sleep(5)
+        await asyncio.sleep(5)
     else:
         logger.error(f"Failed to create job: {job_name}")
         raise kopf.TemporaryError("Job did not complete", delay=30)
 
     logger.info("Database update complete. Setting currentVersion to %s", desired_version)
+
+    deployment_name = "app-deployment"
+    update_deployment_env(namespace, deployment_name, desired_version, logger)
+
     return {'currentVersion': desired_version}
+
+def update_deployment_env(namespace: str, deployment_name: str, new_version: str, logger):
+    apps_api = k8s_client.AppsV1Api()
+
+    patch_body = {
+        "spec" : {
+            "template": {
+                "spec": {
+                    "containers": [
+                        {
+                            "name": "simple-kubernetes-app",
+                            # This patch will replace the container’s existing env list.
+                            # Specify all the env variables the app has.
+                            "env": [
+                                {
+                                    "name": "APP_VERSION",
+                                    "value": new_version
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    }
+
+    try:
+        apps_api.patch_namespaced_deployment(
+            name=deployment_name,
+            namespace=namespace,
+            body=patch_body
+        )
+        logger.info(f"Deployment '{deployment_name}' updated to use APP_VERSION={new_version}s")
+    except Exception as e:
+        logger.error(f"Failed to update deployment '{deployment_name}': {str(e)}")
+        raise
 
 if __name__ == "__main__":
     kopf.run()
--- kubernetes-job/job-script.py
+++ kubernetes-job/job-script.py
@@ -8,7 +8,6 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-
 def simulate_db_update():
     # Get the target DB version from an environment variable.
     target_version = os.getenv("TARGET_DB_VERSION", "unknown")
@@ -26,8 +25,7 @@ def simulate_db_update():
     time.sleep(2)
 
     # Simulate that the update has completed.
-    logging.info("Database update completed successfully. Now at version: %s", target_version)
-
+    logging.info(f"Database update completed successfully. Now at version: {target_version}")
 
 if __name__ == "__main__":
     simulate_db_update()
